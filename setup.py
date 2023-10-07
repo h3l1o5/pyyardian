65145f53a13dcb028ad05a0cbce162ed9b17b6bf
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-07 09:02:04 +0200

    Remove leading whitespace

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Marty Sun",
     author_email="marty.sun@aeonmatrix.com",
     description="A module for interacting with the Yardian irrigation controller",
-    long_description=" Python module for interacting with the Yardian irrigation controller. This module communicates directly towards the IP address of the Yardian device.",
+    long_description="Python module for interacting with the Yardian irrigation controller. This module communicates directly towards the IP address of the Yardian device.",
     long_description_content_type="text/markdown",
     url="https://github.com/h3l1o5/pyyardian",
     install_requires=[
