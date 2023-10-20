65fc2e5550134e91aa122918fa8939e115f48d29
Marty Sun <marty.sun@aeonmatrix.com>
2023-10-20 15:43:36 +0800

    add aiohttp in install_requires

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyyardian",
-    version="1.1.1",
+    version="1.2.0",
     license="MIT",
     author="Marty Sun",
     author_email="marty.sun@aeonmatrix.com",
@@ -11,5 +11,6 @@ setup(
     long_description=" Python module for interacting with the Yardian irrigation controller. This module communicates directly towards the IP address of the Yardian device.",
     long_description_content_type="text/markdown",
     url="https://github.com/h3l1o5/pyyardian",
+    install_requires=["aiohttp"],
     packages=find_packages()
 )
